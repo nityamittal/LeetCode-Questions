cef2ddafeef07ef13f2781727f7874b3907eca4e
Nitya Mittal <56062414+nityamittal@users.noreply.github.com>
2025-02-06 11:33:23 -0500

    Time: 28 ms (52.18%) | Memory: 16.3 MB (67.63%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
 
-    void helper_traverse(vector<vector<char>>& grid, int i, int j, int m, int n)
+    void traverseIsland(vector<vector<char>>& grid, int i, int j, int m, int n)
     {
-        if(i>=m or i<0 or j>=n or j<0 or grid[i][j]=='0')
+        if(i<0 or i>=m or j<0 or j>=n or grid[i][j]=='0')
         return;
 
-            grid[i][j]='0';
-            helper_traverse(grid, i+1, j, m, n);
-            helper_traverse(grid, i, j+1, m, n);
-            helper_traverse(grid, i-1, j, m, n);
-            helper_traverse(grid, i, j-1, m, n);
+        grid[i][j]='0';
+
+        traverseIsland(grid, i+1, j, m, n);
+        traverseIsland(grid, i-1, j, m, n);
+        traverseIsland(grid, i, j+1, m, n);
+        traverseIsland(grid, i, j-1, m, n);
+        return;
     }
+
+
     int numIslands(vector<vector<char>>& grid) 
     {
-        int ans = 0;
-        int m=grid.size();
-        int n=grid[0].size();
+        int numOfIslands = 0;
+
+        int m = grid.size();
+        int n = grid[0].size();
 
         for(int i=0;i<m;++i)
         {
@@ -24,13 +29,17 @@ public:
             {
                 if(grid[i][j]=='1')
                 {
-                    helper_traverse(grid, i, j, m, n);
-                    ++ans;
+                    traverseIsland(grid, i,j,m,n);
+                    ++numOfIslands;
+                }
+                else
+                {
+                    continue;
                 }
             }
-        }
 
-        return ans;
+        }
+        return numOfIslands;
         
     }
 };
