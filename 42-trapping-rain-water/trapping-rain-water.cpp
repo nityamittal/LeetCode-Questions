f794930c8724a0a5af712f386a76197a8f0fea12
Nitya Mittal <56062414+nityamittal@users.noreply.github.com>
2025-08-19 12:38:03 -0400

    Time: 1 ms (25.77%) | Memory: 27.2 MB (31.15%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,31 +2,27 @@ class Solution {
 public:
     int trap(vector<int>& height) 
     {
-        int volume = 0;
-
         int n = height.size();
-        vector<int> left(n,0);
-        vector<int> right(n,0);
+        int vol = 0;
 
-        int left_max = height[0];
-        int right_max = height[n-1];
+        vector<int> left_height(n, 0);
+        vector<int> right_height(n,0);
 
-        for(int i=0,j=n-1;i<n,j>=0;++i,--j)
+        left_height[0] = height[0];
+        right_height[n-1] = height[n-1];
+
+        for(int i = 1;i<n;++i)
         {
-            left_max = max(left_max, height[i]);
-            left[i] = left_max;
-            right_max = max(right_max, height[j]);
-            right[j] = right_max;
+            left_height[i] = max(left_height[i-1], height[i]);
+            right_height[n - 1 - i] = max(right_height[n-i], height[n-1-i]);
         }
 
         for(int i=0;i<n;++i)
         {
-            int max_height = min(left[i], right[i]);
-
-            volume+= max_height-height[i];
-
+            vol+= (min(left_height[i], right_height[i]) - height[i]);
         }
-        return volume;
+
+        return vol;
         
     }
 };
