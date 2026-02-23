6afcb64c4642e5ac9a040ed20d8f4daa50ae931c
Nitya Mittal <56062414+nityamittal@users.noreply.github.com>
2026-02-22 19:28:58 -0500

    Time: 47 ms (67.03%) | Memory: 29.1 MB (45.55%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -2,44 +2,53 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) 
     {
-        set<vector<int>> ans;
         int n = nums.size();
-        sort(nums.begin(), nums.end());
+        vector<vector<int>> ansTriplets;
 
-        for(int i=0;i<n-2;++i)
+        //brute force approach O(n^3)
+        //pick 1 elt + pair sum approach - O(n*n) time + O(n) space
+
+        
+
+        //sort array + pointer approach - O(nlogn + n*n) + O(1) space
+
+        sort(nums.begin(), nums.end());//-4,-1,-1,0,1,2
+        
+
+        //for every elt applying pair sum on the remaining array
+
+        for(int i=0;i<(n-2);++i)//n[i]=-4
         {
-            int l = i+1;
-            int r = n-1;
-            while(l<r)
+            if(i>0 and nums[i]==nums[i-1])
+            continue;
+            
+            int start = i+1;//n[s]=-1
+            int end = n-1;//n[e]=2
+            
+            while(start<end)
             {
-                int sum = nums[i]+nums[l]+nums[r];
+                int sum = nums[i]+nums[start]+nums[end];
                 if(sum==0)
                 {
-                    ans.insert({nums[i],nums[l],nums[r]});
-                    ++l;
-                    --r;
+                    ansTriplets.push_back({nums[i], nums[start], nums[end]});
+                    ++start;
+
+                    while(nums[start]==nums[start-1] and start<end)
+                    ++start;
                 }
-                
-                else if(sum<0)
+                else if(sum>0)
                 {
-                    ++l;
+                    --end;
                 }
                 else
                 {
-                    --r;
+                    ++start;
                 }
             }
         }
 
-        vector<vector<int>> res;
+    return ansTriplets;
 
-        for(auto &s:ans)
-        {
-            res.push_back(s);
-        }
-        return res;
-                
-        
         
     }
 };
